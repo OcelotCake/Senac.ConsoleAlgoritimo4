0a2a7f2d775fc96e8d4a101b14724d82dc37fecc
CHARLESKUNDEVITALE <CHARLESKUNDEVITALE7@gmail.com>
2025-04-01 20:09:29 -0300

    e

--- Algoritimo4/Algoritimo4/Program.cs
+++ Algoritimo4/Algoritimo4/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worlds!");
+using System.Globalization;
+
+Console.WriteLine("Quanto vai gastar na reforma");
+Console.WriteLine("Qual a largura");
+double largura, comprimento, area, valorMetro, valorTerreno;
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Qual o comprimento");
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Qual o valor do metro quadrado");
+valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+area = largura * comprimento;
+valorTerreno = area * valorMetro;
+Console.WriteLine("O valor total é:R$ " + valorTerreno.ToString("F2", CultureInfo.InvariantCulture));
+Console.ReadKey();
+
